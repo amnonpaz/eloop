7637ef7b600d37c66cd7e03581d07814227aba87
paz <Amnon Paz>
2017-04-04 01:05:56 +0300

    Add "Tasks Queue"

---
+++ include/tasks_queue.h
@@ -0,0 +1,28 @@
+#ifndef __TASKS_QUEQUE_H__
+#define __TASKS_QUEQUE_H__
+
+#include "list.h"
+#include <stdbool.h>
+
+typedef void (*task_cb_t)(void *ctx);
+
+typedef unsigned int task_id_t;
+
+#define INVALID_TASK_ID 0
+
+struct tasks_queue;
+
+struct tasks_queue *tasks_queue_new(const char *name);
+
+void tasks_queue_delete(struct tasks_queue *queue, bool execute_all);
+
+task_id_t tasks_queue_add(struct tasks_queue *queue,
+                          const char *name, task_cb_t cb, void *ctx);
+
+char *tasks_queue_task_name(struct tasks_queue *queue, task_id_t id);
+
+void tasks_queue_remove(struct tasks_queue *queue, task_id_t id);
+
+int tasks_queue_execute_next(struct tasks_queue *queue);
+
+#endif /* __TASKS_QUEQUE_H__ */
---
+++ src/tasks_queue.c
@@ -0,0 +1,141 @@
+#include <tasks_queue.h>
+
+#include <stdlib.h>
+#include <string.h>
+
+#define TASK_ID_MIN 1
+#define TASK_ID_MAX ((1 << 16) -1)
+
+struct task {
+    struct list_head list;
+    char *name;
+    task_cb_t cb;
+    void *ctx;
+    task_id_t id;
+};
+
+#define _execute_task(t) \
+    (t)->cb(t->ctx);
+
+struct tasks_queue {
+    struct list_head head;
+    char *name;
+    unsigned int len;
+};
+
+static struct task *task_new(const char *name, task_cb_t cb, void *ctx)
+{
+    static task_id_t id = TASK_ID_MIN;
+
+    struct task *new_task = malloc(sizeof(*new_task));
+    if (!new_task)
+        return NULL;
+
+    list_init(&new_task->list);
+    new_task->name = strdup(name);
+    new_task->cb = cb;
+    new_task->ctx = ctx;
+    new_task->id = id;
+
+    id++;
+    id = (id < TASK_ID_MAX) ? id : TASK_ID_MIN;
+
+    return new_task;
+}
+
+static void task_delete(struct task *rem_task)
+{
+    list_del(&rem_task->list);
+    free(rem_task->name);
+    free(rem_task);
+}
+
+struct tasks_queue *tasks_queue_new(const char *name)
+{
+    struct tasks_queue *queue = malloc(sizeof(*queue));
+    if (!queue)
+        return NULL;
+
+    list_init(&queue->head);
+    queue->name = strdup(name);
+    queue->len = 0;
+
+    return queue;
+}
+
+void tasks_queue_delete(struct tasks_queue *queue, bool execute_all)
+{
+    struct list_head *itr, *item;
+
+    list_for_each_safe(&queue->head, itr, item) {
+        struct task *current_task = (struct task *)item;
+        if (execute_all)
+            _execute_task(current_task);
+
+        task_delete(current_task);
+        queue->len--;
+    }
+
+    free(queue->name);
+    free(queue);
+}
+
+task_id_t tasks_queue_add(struct tasks_queue *queue,
+                          const char *name, task_cb_t cb, void *ctx)
+{
+    struct task *new_task = task_new(name, cb, ctx);
+    if (!new_task)
+        return INVALID_TASK_ID;
+
+    list_add_last(&queue->head, &new_task->list);
+
+    return new_task->id;
+}
+
+static struct task *task_find(struct tasks_queue *queue, task_id_t id)
+{
+    struct list_head *item = NULL, *itr;
+
+    list_for_each(&queue->head, itr) {
+        if (((struct task *)itr)->id != id)
+            continue;
+
+        item = itr;
+        break;
+    }
+
+    return (struct task *)item;
+}
+
+char *tasks_queue_task_name(struct tasks_queue *queue, task_id_t id)
+{
+    struct task *item = task_find(queue, id);
+    if (!item)
+        return NULL;
+
+    return item->name;
+}
+
+void tasks_queue_remove(struct tasks_queue *queue, task_id_t id)
+{
+    struct task *item = task_find(queue, id);
+    if (!item)
+        return;
+
+    task_delete(item);
+    queue->len--;
+}
+
+int tasks_queue_execute_next(struct tasks_queue *queue)
+{
+    struct task *current_task = (struct task *)queue->head.next;
+    if (!current_task)
+        return -1;
+
+    _execute_task(current_task);
+
+    task_delete(current_task);
+    queue->len--;
+
+    return 0;
+}
