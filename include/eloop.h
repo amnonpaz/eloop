152b875e05be37407b87cca3138c8cb74c1f8770
paz <Amnon Paz>
2017-06-01 23:15:31 +0300

    eloop.h: Remove inline function for id compare

--- include/eloop.h
+++ include/eloop.h
@@ -10,17 +10,6 @@
 #define PRIid PRIu64
 typedef uint64_t eloop_id_t;
 
-static inline int eloop_id_cmp(eloop_id_t id1, eloop_id_t id2)
-{
-    if (id1 > id2)
-        return 1;
-
-    if (id1 < id2)
-        return -1;
-
-    return 0;
-}
-
 typedef void (*task_cb_t)(void *ctx);
 
 #endif /* __ELOOP_H__ */
